4a230e8cc85ff8d3244ae3281f307639023d2e5c
Merry <merrychrisd29@gmail.com>
2024-09-16 13:25:12 +0800

    "Updated LoginModal and Navbar components: adjusted logo size, changed navbar colors and text colors, and modified navigation items and styles."

--- geomap/src/components/Navbar.jsx
+++ geomap/src/components/Navbar.jsx
@@ -6,14 +6,13 @@ import LoginModal from './LoginModal';
 import PrjGeomapLogo from '../assets/PrjGeomapLogo.png'
 
 const drawerWidth = 240;
-const navItems = ['Home', 'Features', 'About', 'Contact Us', 'Sign In'];
 
 const Navbar = (props) => {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = useState(false);
 
-  const [navbarColor, setNavbarColor] = useState('transparent');
-  const [navbarTextColor, setNavbarTextColor] = useState('white');
+  const [navbarColor, setNavbarColor] = useState('white');
+  const [navbarTextColor, setNavbarTextColor] = useState('#282F44');
 
   const [loginOpen, setLoginOpen] = useState(false);
 
@@ -26,8 +25,8 @@ const Navbar = (props) => {
         setNavbarColor('#282F44'); // Change to your desired background color
         setNavbarTextColor('#fff'); // Change to your desired text color
       } else {
-        setNavbarColor('#282F44');
-        setNavbarTextColor('white');
+        setNavbarColor('white');
+        setNavbarTextColor('#282F44');
       }
     };
 
@@ -45,19 +44,31 @@ const Navbar = (props) => {
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
       <Typography variant="h6" sx={{ my: 2, color: navbarTextColor }}>
-        <RouterLink to="/">
+        <RouterLink to="/" style={{ textDecoration: 'none', color: navbarTextColor }}>
           Mapulon
         </RouterLink>
       </Typography>
       <Divider />
       <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
+        <RouterLink to="/" style={{ textDecoration: 'none', color: navbarTextColor }}>
+          <ListItem disablePadding>
             <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
+              <ListItemText primary="Home" />
             </ListItemButton>
           </ListItem>
-        ))}
+        </RouterLink>
+        <RouterLink to="/contact" style={{ textDecoration: 'none', color: navbarTextColor }}>
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary="Contact Us" />
+            </ListItemButton>
+          </ListItem>
+        </RouterLink>
+        <ListItem disablePadding onClick={handleLoginOpen}>
+          <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemText primary="Sign In" />
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   );
@@ -68,7 +79,7 @@ const Navbar = (props) => {
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar component="nav" style={{ backgroundColor: navbarColor, transition: 'background-color 0.3s ease-in-out', boxShadow: '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)' }} position='fixed'>
-        <Toolbar width='500px' style={{ color: navbarTextColor }}>
+        <Toolbar sx={{ height: 64, color: navbarTextColor, display: 'flex', justifyContent: 'space-between' }}>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -79,31 +90,22 @@ const Navbar = (props) => {
             <MenuIcon />
           </IconButton>
           <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
-            <img src={PrjGeomapLogo} alt="Mapulon Logo" style={{ height: '120px', marginRight: '1rem' }} />
-            {/* <Typography
-              variant="h6"
-              component="div"
-              sx={{ fontWeight: 700, display: { xs: 'none', sm: 'block' } }}
-            >
-              Mapulon
-            </Typography> */}
+            <img src={PrjGeomapLogo} alt="Mapulon Logo" style={{ height: '50px', marginRight: '1rem' }} />
           </Box>
           <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
             <RouterLink to="/" style={{ textDecoration: 'none' }}>
-              <Button sx={{ marginRight: '4rem', color: '#fff' }}>
+              <Button sx={{ marginRight: '2rem', color: navbarTextColor }}>
                 Home
               </Button>
             </RouterLink>
             <RouterLink to="/contact" style={{ textDecoration: 'none' }}>
-              <Button sx={{ marginRight: '4rem', color: '#fff' }}>
+              <Button sx={{ marginRight: '2rem', color: navbarTextColor }}>
                 Contact Us
               </Button>
             </RouterLink>
-
-            <Button onClick={handleLoginOpen} sx={{ marginRight: '4rem', color: '#fff' }}>
+            <Button onClick={handleLoginOpen} sx={{ color: navbarTextColor }}>
               Sign In
             </Button>
-
             <LoginModal open={loginOpen} handleClose={handleLoginClose} />
           </Box>
         </Toolbar>
